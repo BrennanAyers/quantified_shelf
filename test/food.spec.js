47cc5297a3fd4daf4feae56290ebe744aa4b3b49
Trevor Nodland <trevor.nodland@gmail.com>
2019-08-20 13:50:37 -0600

    updates test to not have open handle

--- test/food.spec.js
+++ test/food.spec.js
@@ -6,17 +6,23 @@ describe('api', () => {
   beforeAll(() => {
     shell.exec('npx sequelize db:create')
   });
-  beforeEach(() => {
+  beforeEach((done) => {
       shell.exec('npx sequelize db:migrate')
       shell.exec('npx sequelize db:seed:all')
+      server = app.listen(3000, (err) => {
+      if (err) return done(err);
+       agent = request.agent(server);
+       done();
     });
-  afterEach(() => {
+  });
+  afterEach((done) => {
     shell.exec('npx sequelize db:migrate:undo:all')
+    return server && server.close(done);
   });
 
   describe('Test food path', () => {
-    test('It can get a single food from the database', () => {
-      return request(app).get('/api/v1/foods/1').then(response => {
+    test('It can get a single food from the database', async () => {
+       await agent.get('/api/v1/foods/1').then(response => {
         expect(response.statusCode).toBe(200)
         expect(response.body.id).toBe(1)
         expect(response.body.name).toBe('Banana')
